9fc1858a9aee4d93815e3815ef10dd07fad9e6dd
Yuu yamanoi <yuu.yamanoi1222@gmail.com>
2022-12-05 23:08:31 +0900

    add input

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,19 @@ import * as core from '@actions/core'
 
 type RunOption = {
   tag?: string
+  targetDir?: string
+  outputDir?: string
 }
 
 const getOption = (): RunOption => {
   const tag = core.getInput('tag')
+  const targetDir = core.getInput('targetDir')
+  const outputDir = core.getInput('outputDir')
 
   return {
-    ...(tag ? { tag } : {})
+    ...(tag ? { tag } : {}),
+    ...(targetDir ? { targetDir } : {}),
+    ...(outputDir ? { outputDir } : {}),
   }
 }
 
@@ -33,11 +39,18 @@ const installDbt2looker = () => {
 const runDbt2looker = (options) => {
   const base = ["dbt2looker"]
 
-  const { tag } = options
+  const { tag, outputDir, targetDir } = options
 
   if (tag) {
     base.push(`--tag=${tag}`)
   }
+  if (targetDir) {
+    base.push(`--target-dir=${targetDir}`)
+  }
+  if (outputDir) {
+    base.push(`--output-dir=${outputDir}`)
+  }
+
   return exec.exec(base.join(' '))
 }
 
